e4dfa3449f890a4749667cdbcfd58e6f3114ab36
Michael Arce <micharcesierra2028@gmail.com>
2020-03-13 17:08:33 -0500

    Inicio del proyecto. Se añadieron 3 escenarios destinados a ser usados en la segunda escena del juego. También se introdujeron los sprites de los personajes principales y de diferentes caballos, estos últimos destinados a ser usados, sobretodo, dentro de un minijuego.

---
+++ AspirantesJuego/Assets/Scripts/AxelController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxelController : MonoBehaviour
+{
+    bool front;
+    bool back;
+
+    private void Start()
+    {
+        front = false;
+        back = false;
+    }
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            front = false;
+            back = false;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(3000f * Time.deltaTime,0,0));
+            gameObject.GetComponent<Animator>().SetBool("lateralMove", true);
+        }    
+        if (Input.GetKey("left"))
+        {
+            front = false;
+            back = false;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(-3000f * Time.deltaTime, 0, 0));
+            gameObject.GetComponent<Animator>().SetBool("lateralMove", true);
+        }
+        if (Input.GetKeyDown("up")) {
+            back = true;
+            front = false;
+            gameObject.GetComponent<Animator>().SetBool("back", true);
+        }
+        if (Input.GetKeyDown("down")) {
+            front = true;
+            back = false;
+            gameObject.GetComponent<Animator>().SetBool("front", true);
+        }
+        if (!Input.GetKey("right") && !Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("lateralMove", false);
+        }
+        if (!front)
+        {
+            gameObject.GetComponent<Animator>().SetBool("front", false);
+        }
+        if (!back)
+        {
+            gameObject.GetComponent<Animator>().SetBool("back", false);
+        }
+    }
+}
---
+++ AspirantesJuego/Assets/Scripts/transition1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transition1 : MonoBehaviour
+{
+    public Camera cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = new Vector3(-10.01f, -24.24f,0);
+            cam.orthographicSize = 8;
+            cam.transform.position = cam.transform.position + new Vector3(-1f,-25f,0);
+        }
+    }
+}
